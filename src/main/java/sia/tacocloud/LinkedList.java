3c6132f7e4894b614b61c8d6b2c33c4536ca266f
Prasahant Verma <v-prverma@expedia.com>
2019-12-08 15:34:32 +0530

    dynamic arrays

---
+++ src/main/java/sia/tacocloud/LinkedList.java
@@ -0,0 +1,11 @@
+package sia.tacocloud;
+
+public class LinkedList {
+    ListNode  head;
+    static class ListNode {
+         int val;
+         ListNode next;
+         ListNode(int x) { val = x; }
+
+    }
+}
--- src/main/java/sia/tacocloud/TacoCloudApplication.java
+++ src/main/java/sia/tacocloud/TacoCloudApplication.java
@@ -1,8 +1,13 @@
 package sia.tacocloud;
 
+import net.bytebuddy.implementation.bind.MethodDelegationBinder;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
 
 
 @SpringBootApplication
@@ -11,6 +16,18 @@ public class TacoCloudApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(TacoCloudApplication.class, args);
+
+        Scanner s = new Scanner(System.in);
+        int input = Integer.valueOf(s.nextLine());
+
+
     }
+
+
+
+
+
+
+
 
 }
