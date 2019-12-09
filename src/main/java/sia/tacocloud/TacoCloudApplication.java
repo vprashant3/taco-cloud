6756f59069f556ea07df8ecd38ada945449e9754
Prasahant Verma <v-prverma@expedia.com>
2019-12-09 16:22:07 +0530

    Doubly Linked List implementation

--- src/main/java/sia/tacocloud/TacoCloudApplication.java
+++ src/main/java/sia/tacocloud/TacoCloudApplication.java
@@ -1,33 +1,26 @@
 package sia.tacocloud;
 
-import net.bytebuddy.implementation.bind.MethodDelegationBinder;
+import com.datastructures.DynamicArray;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Scanner;
+
 
 
 @SpringBootApplication
 @EnableSwagger2
-public class TacoCloudApplication {
+public class TacoCloudApplication<T> {
 
     public static void main(String[] args) {
         SpringApplication.run(TacoCloudApplication.class, args);
 
-        Scanner s = new Scanner(System.in);
-        int input = Integer.valueOf(s.nextLine());
+        DynamicArray<String> dynamicArray = new DynamicArray<>();
+        dynamicArray.add("hi");
+
 
 
     }
-
-
-
-
-
-
-
 
 }
