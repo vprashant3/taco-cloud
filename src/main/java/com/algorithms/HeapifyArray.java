cb4f3c08d689dc293b5c37c2a4794d4997ffd529
Prasahant Verma <v-prverma@expedia.com>
2020-01-06 20:31:34 +0530

    WA soluction

--- src/main/java/com/algorithms/HeapifyArray.java
+++ src/main/java/com/algorithms/HeapifyArray.java
@@ -11,31 +11,29 @@ public class HeapifyArray {
             heapify(arr,arr.length,i);
         }
 
+
+
         Arrays.stream(arr).forEach(System.out::println);
 
     }
 
     private static void heapify(int[] arr, int n, int i)
     {
-        int largest = i; // Initialize largest as root
-        int l = 2 * i + 1; // left = 2*i + 1
-        int r = 2 * i + 2; // right = 2*i + 2
+        int largest = i;
+        int l = 2 * i + 1;
+        int r = 2 * i + 2;
 
-        // If left child is larger than root
         if (l < n && arr[l] > arr[largest])
             largest = l;
 
-        // If right child is larger than largest so far
         if (r < n && arr[r] > arr[largest])
             largest = r;
 
-        // If largest is not root
         if (largest != i) {
             int swap = arr[i];
             arr[i] = arr[largest];
             arr[largest] = swap;
 
-            // Recursively heapify the affected sub-tree
             heapify(arr, n, largest);
         }
     }
