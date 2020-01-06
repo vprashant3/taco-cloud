41a2dfc90a2d721445987fa95d4fd163e63117dd
Prasahant Verma <v-prverma@expedia.com>
2020-01-06 08:57:45 +0530

    Codechef DYNAMO partial solve

--- src/main/java/com/codechef/JAN2020/DYNAMO.java
+++ src/main/java/com/codechef/JAN2020/DYNAMO.java
@@ -13,24 +13,22 @@ public class DYNAMO {
             for (int i = 0; i < T; i++) {
                 int N = sc.nextInt();
                 int A = sc.nextInt();
-                int S = (4*(int)(Math.pow(10,N))+A)/2;
+                int Max = (int) Math.pow(10,N);
+                int S = (2*Max)+A;
                 System.out.println(S);
-                System.out.flush();
                 int B = sc.nextInt();
-                int C = (S - (A+B)) > ((int)Math.pow(10,N)-1)?((int)Math.pow(10,N)-1) : (S - (A+B));
+                int C = Max - B;
                 System.out.println(C);
-                System.out.flush();
                 int D = sc.nextInt();
-                int E = S-(A+B+C+D);
-                E = E>((int)Math.pow(10,N)-1)?((int)Math.pow(10,N)-1) : E;
+                int E = Max - D;
                 System.out.println(E);
-                System.out.flush();
                 int res = sc.nextInt();
-                if(res == -1) System.exit(0);
+                if(res == -1) return;
             }
         } catch (Exception e) {
 
         }
+
 
     }
 }
