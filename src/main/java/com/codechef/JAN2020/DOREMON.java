a6007fe23fcc8d7e582b12b26ba501945bfd0abd
Prasahant Verma <v-prverma@expedia.com>
2020-01-07 11:03:44 +0530

    DOREMON solution

--- src/main/java/com/codechef/JAN2020/DOREMON.java
+++ src/main/java/com/codechef/JAN2020/DOREMON.java
@@ -8,24 +8,26 @@ public class DOREMON {
         try {
             Scanner sc = new Scanner(System.in);
             int T = sc.nextInt();
-            int N = sc.nextInt();
-            int M = sc.nextInt();
 
-            int[][] matrix = new int[M][N];
-            int res = 0;
+            for (int t = 0; t < T; t++) {
+                int N = sc.nextInt();
+                int M = sc.nextInt();
+                int[][] matrix = new int[N][M];
+                int res = 0;
 
-            for(int i=0; i<N; i++) {
-                for(int j=0; j<M; j++) {
-                    matrix[i][j] = sc.nextInt();
+                for (int i = 0; i < N; i++) {
+                    for (int j = 0; j < M; j++) {
+                        matrix[i][j] = sc.nextInt();
+                    }
                 }
-            }
 
-            for(int i=0; i<N; i++) {
-                for(int j=0; j<M; j++) {
-                    res += findAllPaindromesAtThisPoint(matrix,i,j,N,M);
+                for (int i = 0; i < N; i++) {
+                    for (int j = 0; j < M; j++) {
+                        res += findAllPaindromesAtThisPoint(matrix, i, j, N, M);
+                    }
                 }
+                System.out.println(res);
             }
-            System.out.println(res);
 
         } catch (Exception e) {
 
@@ -35,8 +37,8 @@ public class DOREMON {
     private static int findAllPaindromesAtThisPoint(int[][] matrix,int i, int j, int N, int M) {
         int counter = 0;
         while(true) {
-            if(i-counter>=0 && i+counter<N && j-counter>=0 && j+counter<M){
-                if(matrix[i-counter][j] == matrix[i+counter][j] && matrix[i][j-counter]== matrix[i][j+counter]) {
+            if(i-counter>=0 && i+counter<N && j-counter>=0 && j+counter<M) {
+                if(matrix[i-counter][j] == matrix[i+counter][j] && matrix[i][j-counter] == matrix[i][j+counter]) {
                     counter++;
                 }
                 else break;
