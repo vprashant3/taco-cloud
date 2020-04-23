547b0c35d66fd05c1100ec578cbebed107e23e32
Prasahant Verma <v-prverma@expedia.com>
2020-04-23 11:30:10 +0530

    Leet code : random shuffle naive solution

--- src/main/java/com/Runner.java
+++ src/main/java/com/Runner.java
@@ -2,8 +2,22 @@ package com;
 
 import com.datastructures.HashingWithChaining;
 
+import java.util.Optional;
+import java.util.stream.IntStream;
+
 public class Runner {
 
     public static void main(String[] args) {
+        Optional<Boolean> aBoolean = Optional.ofNullable(false);
+
+        aBoolean.ifPresent(x -> System.out.println("true"));
+
+    }
+
+
+
+
+    private static int next2Power(int capacity) {
+        return Integer.highestOneBit(capacity) << 1;
     }
 }
---
+++ src/main/java/com/leetcode/ArrayProblems/ShuffleAnArray.java
@@ -0,0 +1,36 @@
+package com.leetcode.ArrayProblems;
+
+
+import java.util.Random;
+import java.util.stream.IntStream;
+
+public class ShuffleAnArray {
+
+    public static void main(String[]  args) {
+       int[] inputArray = {2, 3, 4, 5, 6, 7};
+       int[] outputArray =  shuffledArray(inputArray);
+       for(int i = 0; i< outputArray.length; i++) {
+           System.out.println(outputArray[i]);
+       }
+    }
+
+
+    // take a random value from inputArray, assign to outputArray, then remove the random value from inputarray and reduce its size
+    // time complexity O(n2)
+    private static int[] shuffledArray(int[] inputArray) {
+        Random r = new Random();
+        int[] outputArray = new int[inputArray.length];
+        int[] reducedArray = inputArray;
+        for(int i=0; i<inputArray.length; i++) {
+            int length = reducedArray.length;
+            int randomLocation = r.nextInt(length);
+            outputArray[i] = reducedArray[randomLocation];
+            int[] finalReducedArray = reducedArray;
+            int[] tempArray = IntStream.range(0,length).filter(loc -> loc != randomLocation).map(loc -> finalReducedArray[loc]).toArray();
+            reducedArray = tempArray;
+        }
+
+
+        return outputArray;
+    }
+}
