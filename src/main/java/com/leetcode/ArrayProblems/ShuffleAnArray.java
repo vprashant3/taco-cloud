ba7b47fbbe636a483f0e510597378eb66e31f19c
Prasahant Verma <v-prverma@expedia.com>
2020-04-23 11:48:35 +0530

    Leet code : random shuffle optimized fisher yates solution

--- src/main/java/com/leetcode/ArrayProblems/ShuffleAnArray.java
+++ src/main/java/com/leetcode/ArrayProblems/ShuffleAnArray.java
@@ -8,13 +8,27 @@ public class ShuffleAnArray {
 
     public static void main(String[]  args) {
        int[] inputArray = {2, 3, 4, 5, 6, 7};
-       int[] outputArray =  shuffledArray(inputArray);
-       for(int i = 0; i< outputArray.length; i++) {
-           System.out.println(outputArray[i]);
-       }
+//       int[] outputArray =  shuffledArray(inputArray);
+//       for(int i = 0; i< outputArray.length; i++) {
+//           System.out.println(outputArray[i]);
+//       }
+       int[] optimizedSolution =  optimizedShuffling(inputArray);
+        for(int i = 0; i< optimizedSolution.length; i++) {
+            System.out.println(optimizedSolution[i]);
+        }
     }
 
+    private static int[] optimizedShuffling(int[] inputArray) {
+        Random r = new Random();
+        for( int i = inputArray.length-1; i>=0; i--) {
+            int randomLoc = r.nextInt(i+1);
 
+            int temp = inputArray[randomLoc];
+            inputArray[randomLoc] = inputArray[i];
+            inputArray[i] = temp;
+        }
+        return inputArray;
+    }
 
 
     // take a random value from inputArray, assign to outputArray, then remove the random value from inputarray and reduce its size
