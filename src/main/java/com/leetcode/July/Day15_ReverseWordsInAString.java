86890b4237f327782126507697c5abb7cd494d0c
Prasahant Verma <v-prverma@expedia.com>
2020-07-16 08:40:37 +0530

    Leet Code July Challenge Day 15 : Reverse Words in a string

---
+++ src/main/java/com/leetcode/July/Day15_ReverseWordsInAString.java
@@ -0,0 +1,16 @@
+package com.leetcode.July;
+
+public class Day15_ReverseWordsInAString {
+    public static void main(String[] args) {
+       String rev=  new Day15_ReverseWordsInAString().reverseWords("the sky is blue");
+       System.out.println(rev);
+    }
+
+    public String reverseWords(String s) {
+        String[] words = s.trim().split(" +");
+        StringBuilder sb = new StringBuilder();
+        for(int i = words.length - 1; i > 0; i--)
+            sb.append(words[i] + " ");
+        return sb.append(words[0]).toString();
+    }
+}
