0ec48290321f00950fd22a799648a8319533512d
Prasahant Verma <v-prverma@expedia.com>
2020-07-23 14:08:05 +0530

    Leet Code : July Challenge Day 22

---
+++ src/main/java/com/LeetCode/July/Day23_SingleNumberIII.java
@@ -0,0 +1,16 @@
+package com.LeetCode.July;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class Day23_SingleNumberIII {
+
+    public int[] singleNumber(int[] nums) {
+        Set<Integer> res = new HashSet<>();
+        for(int i :  nums) {
+            if(res.contains(i)) res.remove(i);
+            else res.add(i);
+        }
+        return res.stream().mapToInt(Integer::intValue).toArray();
+    }
+}
