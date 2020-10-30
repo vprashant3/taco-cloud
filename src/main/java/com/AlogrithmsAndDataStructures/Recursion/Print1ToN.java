b44a7702cc60c57112e001d00d05cda211cc9c38
Prasahant Verma <v-prverma@expedia.com>
2020-10-30 09:38:17 +0530

    factorial recursive

--- src/main/java/com/AlogrithmsAndDataStructures/Recursion/Print1ToN.java
+++ src/main/java/com/AlogrithmsAndDataStructures/Recursion/Print1ToN.java
@@ -3,7 +3,8 @@ package com.AlogrithmsAndDataStructures.Recursion;
 public class Print1ToN {
 
     public static void main(String[] args) {
-        new Print1ToN().print(10);
+//        new Print1ToN().print(10);
+        System.out.println(new Print1ToN().factorialRecursive(5));
     }
 
     private void  print(int n) {
@@ -11,5 +12,10 @@ public class Print1ToN {
             print(n-1);
             System.out.println(n);
         }
+    }
+
+    private int factorialRecursive(int n) {
+        if(n == 1) return 1;
+        return n * factorialRecursive(n - 1);
     }
 }
