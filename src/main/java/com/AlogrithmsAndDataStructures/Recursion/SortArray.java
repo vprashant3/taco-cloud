e0f507dce436b69bec9176964dc20c7eba08e345
Prasahant Verma <v-prverma@expedia.com>
2020-10-31 11:29:32 +0530

    Sort Array Using Recursion + merge using recursion as well

--- src/main/java/com/AlogrithmsAndDataStructures/Recursion/SortArray.java
+++ src/main/java/com/AlogrithmsAndDataStructures/Recursion/SortArray.java
@@ -5,7 +5,7 @@ import java.util.Arrays;
 public class SortArray {
 
     public static void main(String[] args) {
-        int[] ip = {6,3,2,5,2,1};
+        int[] ip = {3,2,1,1,5,6,7};
         int[] op = sortArrayUsingRecursion(ip, ip.length);
         Arrays.stream(op).forEach(System.out::println);
     }
@@ -13,8 +13,26 @@ public class SortArray {
     private static int[] sortArrayUsingRecursion(int[] ip, int len) {
         if(len == 1) return ip;
         int[] sortedArray = sortArrayUsingRecursion(Arrays.copyOfRange(ip,0,len - 1), len -1);
-        int[] res = merge(sortedArray, ip[len - 1]);
+        int[] res = mergeUsingRecursion(sortedArray, ip[len - 1]);
         return res;
+    }
+
+    private static int[] mergeUsingRecursion(int[] sortedArray, int i) {
+        int[] mergedArray = new int[sortedArray.length + 1];
+        if(sortedArray.length == 0) {
+            mergedArray[0] = i;
+        } else if( sortedArray[sortedArray.length - 1] <= i) {
+            mergedArray = Arrays.copyOf(sortedArray, sortedArray.length + 1);
+            mergedArray[sortedArray.length] = i;
+        } else {
+            int[] intermediateMerge = mergeUsingRecursion(Arrays.copyOfRange(sortedArray, 0,sortedArray.length - 1), i);
+            mergedArray = Arrays.copyOf(intermediateMerge, intermediateMerge.length + 1);
+            mergedArray[mergedArray.length - 1] =  sortedArray[sortedArray.length - 1];
+        }
+        return mergedArray;
+
+
+
     }
 
     private static int[] merge(int[] sortedArray, int lastElement) {
