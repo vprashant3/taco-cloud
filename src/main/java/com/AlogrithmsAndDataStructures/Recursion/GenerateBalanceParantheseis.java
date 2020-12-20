e663e8899348b9317185bd316e7cb23c35e60434
Prasahant Verma <v-prverma@expedia.com>
2020-12-20 10:33:32 +0530

    starting with DP again

--- src/main/java/com/AlogrithmsAndDataStructures/Recursion/GenerateBalanceParantheseis.java
+++ src/main/java/com/AlogrithmsAndDataStructures/Recursion/GenerateBalanceParantheseis.java
@@ -1,15 +1,18 @@
 package com.AlogrithmsAndDataStructures.Recursion;
 
+import java.util.Arrays;
+import java.util.Collections;
 import java.util.HashSet;
 import java.util.Set;
 
 public class GenerateBalanceParantheseis {
 
     public static void main(String[] args) {
-        Set<String> strings =  new HashSet<>();
-        int num = 3;
-        String output = "";
-        generate(3, 3, output);
+      int[] input =  {0,1,0,2,0,1,2,0};
+        int[] ints = new GenerateBalanceParantheseis().sortArray(input);
+
+        Arrays.stream(ints).forEach(System.out::println);
+
     }
 
     private static void generate(int openRemaining, int closedRemaining, String output) {
@@ -28,4 +31,20 @@ public class GenerateBalanceParantheseis {
             }
         }
     }
+
+
+
+    // key idea here is sort the arrays then swap adjacent elements to get wave form
+    // time complexity would would O(nlogn) and space complexity is connstant as no additional memory is used.
+    int[] sortArray(int[] input){
+        // sorting is nlogn
+        Arrays.sort(input);
+        for(int i = 0; i < input.length - 1; i = i + 2) {
+            int temp = input[i];
+            input[i] = input[i+1];
+            input[i + 1] = temp;
+        }
+        return a;
+    }
+
 }
