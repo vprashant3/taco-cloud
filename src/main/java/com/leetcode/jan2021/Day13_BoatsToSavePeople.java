680da813750138a8d07dea27b57e7f835585a767
Prasahant Verma <v-prverma@expedia.com>
2021-01-13 20:12:58 +0530

    13th Jan 2021 leetcode daily questiion

---
+++ src/main/java/com/leetcode/jan2021/Day13_BoatsToSavePeople.java
@@ -0,0 +1,27 @@
+package com.leetcode.jan2021;
+
+import java.util.Arrays;
+
+public class Day13_BoatsToSavePeople {
+
+    public static void main(String[] args) {
+
+    }
+
+    public int numRescueBoats(int[] people, int limit) {
+        if(people.length == 1) return 1;
+
+        int count = 0;
+        int i = 0 ;
+        int j = people.length - 1;
+        Arrays.sort(people);
+
+        while(i <= j) {
+            if((j-1 >= 0) && (people[j] + people[j -1] <= limit)) j -= 2;
+            else if(people[j] + people[i] <= limit) { i++; j--;}
+            else j--;
+            count++;
+        }
+        return count;
+    }
+}
