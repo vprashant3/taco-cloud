b869e0a8b543d2256728cd6388b0f0dafa886b5e
Prasahant Verma <v-prverma@expedia.com>
2021-01-18 15:54:20 +0530

    leetcode daily

---
+++ src/main/java/com/leetcode/jan2021/Day18_MaxNumberOFKSumPairs.java
@@ -0,0 +1,32 @@
+package com.leetcode.jan2021;
+
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.HashSet;
+
+public class Day18_MaxNumberOFKSumPairs {
+
+    public static void main(String[] args) {
+        int k = 5;
+        int[] nums = {1,2,3,4};
+        new Day18_MaxNumberOFKSumPairs().maxOperations(nums,k);
+    }
+
+    public int maxOperations(int[] nums, int k) {
+        Arrays.sort(nums);
+        int left = 0, right = nums.length - 1, pairs = 0;
+
+        while(left < right) {
+            if(nums[left] + nums[right] == k) {
+                left++;
+                right--;
+                pairs++;
+            } else if(nums[left] + nums[right] > k) {
+                right--;
+            } else {
+                left++;
+            }
+        }
+        return pairs;
+    }
+}
